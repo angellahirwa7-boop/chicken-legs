4ddb29c6b6a5f3c57ecec7c27e82efb836798c96
angellahirwa7-boop <angellahirwa7@gmail.com>
2025-10-18 09:49:06 -0700

    Add files via upload

---
+++ prime1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+   int n, i, isprime, limit;
+   for(n=2; n<=100; n++){
+      isprime = 1;
+      limit=sqrt (n);
+      for (i=2;i <= limit;i++){
+      if(n % i==0){
+      isprime = 0;
+      break;
+      }
+      }
+      if (isprime==1){
+      printf("%d " ,n);
+      }
+      }
+      return 0;
+      }
+
